bfdd14600e6d6437882932238f80c9b1c6bd0787
bkjam <code4bryan@gmail.com>
2022-05-29 03:49:48 +0800

    fix logic in RedisStreamConsumer

--- 02-scaling-websocket-server/websocket-server/src/main/kotlin/com/example/websocket/service/RedisStreamConsumer.kt
+++ 02-scaling-websocket-server/websocket-server/src/main/kotlin/com/example/websocket/service/RedisStreamConsumer.kt
@@ -1,15 +1,15 @@
 package com.example.websocket.service
 
 import com.example.common.model.StreamDataEvent
+import com.example.websocket.model.BroadcastEvent
 import org.slf4j.LoggerFactory
 import org.springframework.data.redis.connection.stream.ObjectRecord
 import org.springframework.data.redis.stream.StreamListener
-import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class RedisStreamConsumer(
-    private val websocketTemplate: SimpMessagingTemplate
+    private val redisBroadcastService: RedisBroadcastService
 ): StreamListener<String, ObjectRecord<String, StreamDataEvent>> {
     companion object {
         private val logger = LoggerFactory.getLogger(RedisStreamConsumer::class.java)
@@ -18,7 +18,8 @@ class RedisStreamConsumer(
     override fun onMessage(record: ObjectRecord<String, StreamDataEvent>) {
         logger.info("[NEW] --> received message: ${record.value} from stream: ${record.stream}")
         record.value.topic?.let { destination ->
-            websocketTemplate.convertAndSend(destination, record.value.message)
+            val broadcastEvent = BroadcastEvent(destination, record.value.message)
+            redisBroadcastService.publish(broadcastEvent)
         }
     }
 }
